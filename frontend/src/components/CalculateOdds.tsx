7cd7cbcf6756bb615a778bee3dc2307e6ab53c0c
Guilhermo Rubinstein Tavares <rubinstein.g@sfeir.com>
2023-12-02 00:50:33 +0100

    bugfix(front) put axios in useEffect to avoid multiple calls

--- frontend/src/components/CalculateOdds.tsx
+++ frontend/src/components/CalculateOdds.tsx
@@ -1,14 +1,18 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import { OddsCalculationResult } from "../types/OddsCalculationResult";
 
 export const CalculateOdds = () => {
     const [odds, setOdds] = useState<number>()
     const [canEscape, setCanEscape] = useState<boolean>()
-    axios.get<OddsCalculationResult>("http://localhost:8080", {}).then(res => {
-        setOdds(res.data.oddsPercentage)
-        setCanEscape(res.data.canEscape)
-    })
+    useEffect(() => {
+        axios.get<OddsCalculationResult>("http://localhost:8080", {}).then((res) => {
+            setOdds(res.data.oddsPercentage)
+            setCanEscape(res.data.canEscape)
+        })
+
+        return () => {}
+    }, [])
 
     return <>
         {!odds ? 
