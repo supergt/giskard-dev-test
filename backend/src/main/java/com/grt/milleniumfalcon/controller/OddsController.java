ef2caccbb54f27bb65fb5963bab97f72e5b55ee0
Guilhermo Rubinstein Tavares <rubinstein.g@sfeir.com>
2023-12-01 23:02:23 +0100

    feature(back) create result object

--- backend/src/main/java/com/grt/milleniumfalcon/controller/OddsController.java
+++ backend/src/main/java/com/grt/milleniumfalcon/controller/OddsController.java
@@ -1,12 +1,20 @@
 package com.grt.milleniumfalcon.controller;
 
+import com.grt.milleniumfalcon.dto.OddsCalculationResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
+@CrossOrigin(origins = "*")
 @RestController
 public class OddsController {
     @GetMapping("")
-    public String example() {
-        return "Hello world";
+    public OddsCalculationResult calculateOdds() {
+        return OddsCalculationResult.builder()
+                .oddsPercentage(new BigDecimal("0.5"))
+                .canEscape(false)
+                .build();
     }
 }
