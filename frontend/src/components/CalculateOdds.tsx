bac10be1aba38d2fdfaf97db94d54c43fbb47ab7
Guilhermo Rubinstein Tavares <rubinstein.g@sfeir.com>
2023-12-02 01:10:01 +0100

    feature(front) send stolen plans json to backend to calculate odds

--- frontend/src/components/CalculateOdds.tsx
+++ frontend/src/components/CalculateOdds.tsx
@@ -1,18 +1,39 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import { OddsCalculationResult } from "../types/OddsCalculationResult";
+import { StolenPlans } from "../types/StolenPlans";
 
 export const CalculateOdds = () => {
     const [odds, setOdds] = useState<number>()
     const [canEscape, setCanEscape] = useState<boolean>()
+    const [stolenPlans, setStolenPlans] = useState<StolenPlans>()
+
     useEffect(() => {
-        axios.get<OddsCalculationResult>("http://localhost:8080", {}).then((res) => {
-            setOdds(res.data.oddsPercentage)
-            setCanEscape(res.data.canEscape)
+        setStolenPlans({
+            countdown: 6,
+            bounty_hunters: [
+                {
+                    planet: "Tatooine",
+                    day: 4,
+                },
+                {
+                    planet: "Dagobah",
+                    day: 5,
+                },
+            ]
         })
+    }, [])
+
+    useEffect(() => {
+        if (stolenPlans) {
+            axios.post<OddsCalculationResult>("http://localhost:8080", stolenPlans).then((res) => {
+                setOdds(res.data.oddsPercentage)
+                setCanEscape(res.data.canEscape)
+            })
+        }
 
         return () => {}
-    }, [])
+    }, [stolenPlans])
 
     return <>
         {!odds ? 
---
+++ frontend/src/types/StolenPlans.ts
@@ -0,0 +1,4 @@
+export type StolenPlans = {
+    countdown: number
+    bounty_hunters: { planet: string, day: number }[]
+}
