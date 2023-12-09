bc3c1bd520cf9106e10cca3bddfe38144d6c72c9
Guilhermo Rubinstein Tavares <rubinstein.g@sfeir.com>
2023-12-09 16:31:20 +0100

    feature(front) show escape plan steps

--- frontend/src/components/CalculateOdds.tsx
+++ frontend/src/components/CalculateOdds.tsx
@@ -1,23 +1,28 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-import { OddsCalculationResult } from "../types/OddsCalculationResult";
+import { EscapePlanStep, OddsCalculationResult } from "../types/OddsCalculationResult";
 import { StolenPlans } from "../types/StolenPlans";
 
 export const CalculateOdds = () => {
     const [odds, setOdds] = useState<number>()
     const [stolenPlans, setStolenPlans] = useState<StolenPlans>()
+    const [escapePlans, setEscapePlans] = useState<EscapePlanStep[]>()
 
     useEffect(() => {
         setStolenPlans({
-            countdown: 6,
+            countdown: 10,
             bounty_hunters: [
                 {
-                    planet: "Tatooine",
-                    day: 4,
+                    planet: "Hoth",
+                    day: 6,
                 },
                 {
-                    planet: "Dagobah",
-                    day: 5,
+                    planet: "Hoth",
+                    day: 7,
+                },
+                {
+                    planet: "Hoth",
+                    day: 8,
                 },
             ]
         })
@@ -27,6 +32,7 @@ export const CalculateOdds = () => {
         if (stolenPlans) {
             axios.post<OddsCalculationResult>("http://localhost:8080", stolenPlans).then((res) => {
                 setOdds(res.data.oddsPercentage)
+                setEscapePlans(res.data.escapePlanSteps)
             })
         }
 
@@ -34,10 +40,25 @@ export const CalculateOdds = () => {
     }, [stolenPlans])
 
     return <>
-        {!odds ? 
+        {odds === undefined
+        ? 
             <>Calculating odds...</>
-        : <>
-            <p>Odds calculated - Millennium Falcon has {odds}% chance of escaping.</p>
-        </>}
-    </>
+        : 
+            <>
+                <p>Odds calculated - Millennium Falcon has {odds}% chance of escaping.</p>
+                {!!escapePlans && (
+                    <ol>
+                        {escapePlans.map(ep => (
+                            <li key={ep.startDay}>
+                                On day {ep.startDay},
+                                {!!ep.endPlanet
+                                    ? (<> travel from {ep.startPlanet} to {ep.endPlanet}</>)
+                                    : (<> refuel on {ep.startPlanet}</>)
+                                }
+                            </li>
+                        ))}
+                    </ol>
+                )}
+            </>}
+        </>
 }
--- frontend/src/types/OddsCalculationResult.ts
+++ frontend/src/types/OddsCalculationResult.ts
@@ -1,3 +1,12 @@
 export type OddsCalculationResult = {
     oddsPercentage: number
+    escapePlanSteps: EscapePlanStep[]
+}
+
+export type EscapePlanStep = {
+    startPlanet: string
+    startDay: number
+    endPlanet?: string
+    endDay: number
+    refuel: boolean
 }
